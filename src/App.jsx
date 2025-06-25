7016323c25923f50fe953a37c5882a3a955bba23
Glêsio Santos <glesioss@gmail.com>
2025-06-25 17:09:37 -0300

    configurando prettier e criando e alterando o status da tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTasks from './components/AddTask'
+import Tasks from './components/Task'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar React',
+      description: 'Aprender a usar o react para o dia a dia',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Estudar Mercado',
+      description: 'Aprender as ferramentas do mercado financeiro',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Desenvolver sistema de orçamento de serviços',
+      description: 'Elaborar o sistema de controle de orçamentos de serviços',
+      isCompleted: false
+    }
+  ])
+
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task
+    })
+
+    setTasks(newTask)
+  }
+
+  function onDeleteTask(taskId) {
+    const newTask = tasks.filter((task) => task.id !== taskId)
+    setTasks(newTask)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTasks />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTask={onDeleteTask}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
