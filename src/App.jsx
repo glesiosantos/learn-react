c9fb67b6626802f1e74f4427420ab0d6261812f7
Glêsio Santos <glesioss@gmail.com>
2025-06-25 21:24:21 -0300

    Finalizano cadastro de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import { useState } from 'react'
+import {v4} from 'uuid'
+
 import AddTasks from './components/AddTask'
 import Tasks from './components/Task'
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Estudar React',
-      description: 'Aprender a usar o react para o dia a dia',
-      isCompleted: false
-    },
-    {
-      id: 2,
-      title: 'Estudar Mercado',
-      description: 'Aprender as ferramentas do mercado financeiro',
-      isCompleted: false
-    },
-    {
-      id: 3,
-      title: 'Desenvolver sistema de orçamento de serviços',
-      description: 'Elaborar o sistema de controle de orçamentos de serviços',
+  const [tasks, setTasks] = useState([])
+
+  function onAddTaskToList(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
       isCompleted: false
     }
-  ])
+
+    setTasks([...tasks, newTask])
+  }
 
   function onTaskClick(taskId) {
     const newTask = tasks.map((task) => {
@@ -42,11 +36,11 @@ function App() {
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTasks />
+        <AddTasks onAddTaskToList={onAddTaskToList}/>
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,38 @@
-function AddTasks() {
-  return <h1>Add Tasks</h1>
+import { Proportions } from "lucide-react"
+import { useState } from "react"
+
+function AddTasks({onAddTaskToList}) {
+
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col my-4">
+      <input type="text"
+        value={title} 
+        onChange={(event) => setTitle(event.target.value)}
+        placeholder="Digita o titulo da tarefa" 
+        className="border border-slate-300 outline-slade-300 px-4 py-2 rounded-md" />
+      <textarea 
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        rows="3" 
+        placeholder="Descrição da tarefa" className="border border-slate-300 outline-slade-300 px-4 py-2 rounded-md"></textarea>
+      <button 
+        onClick={() => {
+
+          if (!title.trim() || !description.trim()) {
+            alert('Preencha o titulo ou a descrição da tarefa')
+          } else {
+            onAddTaskToList(title, description)
+            setTitle('')
+            setDescription('')
+          }
+
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+    </div>
+  )
 }
 
 export default AddTasks
